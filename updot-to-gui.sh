67d465ea66d65224a5eec3e3f5f93f192227052e
William Swanson <swansontec@gmail.com>
2019-12-02 14:35:52 -0800

    updot test

---
+++ updot-to-gui.sh
@@ -0,0 +1,5 @@
+cp types.js.flow ../edge-react-gui/node_modules/edge-core-js/
+cp index.js.flow ../edge-react-gui/node_modules/edge-core-js/
+cp error.js.flow ../edge-react-gui/node_modules/edge-core-js/
+cp src/types/entries.js.flow ../edge-react-gui/node_modules/edge-core-js/src/types/entries.js.flow
+rm ../edge-react-gui/node_modules/edge-core-js/src/types/types.js
